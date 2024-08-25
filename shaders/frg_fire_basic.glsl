7b3968affb37c5b6d0bff00f3db8b4178f0d4aed
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-08-25 17:08:35 +0200

    Add various parameter knobs

--- shaders/frg_fire_basic.glsl
+++ shaders/frg_fire_basic.glsl
@@ -25,19 +25,27 @@ SOFTWARE.
 layout(location = 0) out vec4 outputCoeff;
 layout(location = 1) out vec4 outputColor;
 
-uniform float uTimeMs;
+float PI = 3.141;
+
 uniform sampler2D uInputCoeffs;
 
-float PI = 3.141;
-float DOWNCREASE_MIN = 0.005;
+uniform bool uPlay;
+uniform float uTimeAccMs;
+uniform float uTimeDeltaMs;
+uniform float uDiffusionCoeff;
+uniform float uVerticalForce;
+uniform float uDissipationMinimum;
 
-vec3 palette_coeff_a = vec3(0.5, 0.5, 0.5);
-vec3 palette_coeff_b = vec3(0.5, 0.5, 0.5);
-vec3 palette_coeff_c = vec3(1.0, 1.0, 0.5);
-vec3 palette_coeff_d = vec3(0.79, 0.88, 0.29);
+uniform vec3 uPaletteLuminosity;
+uniform vec3 uPaletteContrast;
+uniform vec3 uPaletteFreq;
+uniform vec3 uPalettePhase;
+
 vec3 color_palette(float val) {
-    // From Inigo Quilez - https://www.youtube.com/shorts/TH3OTy5fTog
-    return palette_coeff_a + palette_coeff_b*cos(2.0*PI*(val*palette_coeff_c+palette_coeff_d));
+    // From Inigo Quilez
+    // https://www.youtube.com/shorts/TH3OTy5fTog
+    // https://iquilezles.org/articles/palettes/
+    return uPaletteLuminosity + uPaletteContrast*cos(2.0*PI*(val*uPaletteFreq+uPalettePhase));
 }
 
 float random(vec2 uv) {
@@ -55,22 +63,30 @@ float average_noise_smoothstep(vec2 scaled_uv) {
     return mix(top_avg, bottom_avg, percent.y);
 }
 
+void renderDisplayedTexture() {
+    outputColor = vec4(color_palette(outputCoeff.r), 1.0);
+}
+
 void main() {
     outputCoeff.r = texelFetch(uInputCoeffs, ivec2(gl_FragCoord.xy), 0).r;
+    if (! uPlay) {
+        renderDisplayedTexture();
+        return;
+    }
     float leftCoeff =  texelFetch(uInputCoeffs, ivec2(gl_FragCoord.xy) + ivec2(-1, 0), 0).r;
     float rightCoeff = texelFetch(uInputCoeffs, ivec2(gl_FragCoord.xy )+ ivec2(1, 0), 0).r;
     float topCoeff = texelFetch(uInputCoeffs, ivec2(gl_FragCoord.xy) + ivec2(0, 1), 0).r;
     float downCoeff = 0.0;
     if (gl_FragCoord.y < 1.0) {
-        downCoeff = average_noise_smoothstep(vec2(gl_FragCoord.x/10.0, uTimeMs/100.0));
+        downCoeff = average_noise_smoothstep(vec2(gl_FragCoord.x/10.0, uTimeAccMs/100.0));
     }
     else {
         downCoeff = texelFetch(uInputCoeffs, ivec2(gl_FragCoord.xy) + ivec2(0, -1), 0).r;
     }
-    float coeffDelta = 0.1*(5.0*downCoeff + leftCoeff + rightCoeff + topCoeff - 8.0*outputCoeff.r);
-    if (coeffDelta < 0.0 && coeffDelta >= -DOWNCREASE_MIN){
-        coeffDelta = -DOWNCREASE_MIN;
+    float coeffDelta = uDiffusionCoeff*(uTimeDeltaMs/1000.0)*(uVerticalForce*downCoeff + leftCoeff + rightCoeff + topCoeff - (3.0 + uVerticalForce)*outputCoeff.r);
+    if (coeffDelta < 0.0 && coeffDelta >= -uDissipationMinimum){
+        coeffDelta = -uDissipationMinimum;
     } 
     outputCoeff.r += coeffDelta;
-    outputColor = vec4(color_palette(outputCoeff.r), 1.0);
+    renderDisplayedTexture();
 }
--- src/main.js
+++ src/main.js
@@ -33,11 +33,17 @@ const FShaderFireBasic = await textFileLoader("/shaders/frg_fire_basic.glsl");
 const params = {
     canvasResolution: 50, // In percentage
     canvasScale: true,
+    canvasSmooth: false,
     fpsDisplay: false,
+    paletteLuminosity: { x: 1.0, y: 1.0, z: 0.1 },
+    paletteContrast: { x: 1.0, y: 1.0, z: 1.0 },
+    paletteFreq: { x: 2.0, y: 0.5, z: 0.5 },
+    palettePhase: { x: 0.5, y: 0.5, z: 0.5 },
+    animRun: true,
+    verticalForce: 5.0,
+    dissipationMinimum: 0.01,
+    diffusionCoeff: 6,
 };
-
-// Base time to pass time delta as uniform to shaders
-const baseTime = Date.now();
 
 // Common Three.js elements
 const canvasGeometry = new THREE.PlaneGeometry(1, 1);
@@ -52,14 +58,23 @@ const stats = new Stats()
 document.body.appendChild(stats.dom);
 
 // Step 1 rendering
-let inputRenderTarget = new THREE.WebGLRenderTarget(undefined, undefined, {count: 2});
-let outputRenderTarget = new THREE.WebGLRenderTarget(undefined, undefined, {count: 2}); // channel 0 -> coefficients, channel 1 -> colors to display
+let inputRenderTarget = new THREE.WebGLRenderTarget(undefined, undefined, { count: 2 });
+let outputRenderTarget = new THREE.WebGLRenderTarget(undefined, undefined, { count: 2 }); // channel 0 -> coefficients, channel 1 -> colors to display
 const materialStep1 = new THREE.ShaderMaterial({
     fragmentShader: FShaderFireBasic,
     glslVersion: THREE.GLSL3,
     uniforms: {
         uInputCoeffs: { value: inputRenderTarget.textures[0] },
-        uTimeMs: { value: 0.0 },
+        uTimeAccMs: { value: 0.0 },
+        uTimeDeltaMs: { value: 0.0 },
+        uPlay: { value: true },
+        uVerticalForce: { value: 0.0 },
+        uDissipationMinimum:  { value: 0.0 },
+        uDiffusionCoeff: { value: 0.0 },
+        uPaletteLuminosity: { value: new THREE.Vector3(0.0, 0.0, 0.0) },
+        uPaletteContrast: { value: new THREE.Vector3(0.0, 0.0, 0.0) },
+        uPaletteFreq: { value: new THREE.Vector3(0.0, 0.0, 0.0) },
+        uPalettePhase: { value: new THREE.Vector3(0.0, 0.0, 0.0) },
     }
 });
 const sceneStep1 = new THREE.Scene();
@@ -80,25 +95,46 @@ const switchStep1RenderTargets = () => {
 
 // Configure elements depending on window and Tweakpane
 const applyDisplayParams = () => {
+    const paramPoint3ToVector3 = (paramPoint3Val) => new THREE.Vector3(
+        paramPoint3Val.x, paramPoint3Val.y, paramPoint3Val.z
+    );
     const newWidth = window.innerWidth * params.canvasResolution / 100;
     const newHeight = window.innerHeight * params.canvasResolution / 100;
     inputRenderTarget.setSize(newWidth, newHeight);
     outputRenderTarget.setSize(newWidth, newHeight);
     renderer.setSize(newWidth, newHeight);
     if (params.canvasScale) {
-        renderer.domElement.style.cssText = "width: 100%; margin:0; padding: 0;  image-rendering: pixelated";
+        renderer.domElement.style.cssText = "width: 100%; margin:0; padding: 0;";
+        if (!params.canvasSmooth) {
+            renderer.domElement.style.cssText += "image-rendering: pixelated"
+        }
     }
     stats.dom.hidden = !params.fpsDisplay;
+    materialStep1.uniforms.uPaletteLuminosity.value = paramPoint3ToVector3(params.paletteLuminosity);
+    materialStep1.uniforms.uPaletteContrast.value = paramPoint3ToVector3(params.paletteContrast);
+    materialStep1.uniforms.uPaletteFreq.value = paramPoint3ToVector3(params.paletteFreq);
+    materialStep1.uniforms.uPalettePhase.value = paramPoint3ToVector3(params.palettePhase);
+    materialStep1.uniforms.uVerticalForce.value = params.verticalForce;
+    materialStep1.uniforms.uDissipationMinimum.value = params.dissipationMinimum;
+    materialStep1.uniforms.uDiffusionCoeff.value = params.diffusionCoeff;
 }
 applyDisplayParams();
 
 // Rendering
+let timeAcc = 0;
+let timeDatePrev =  Date.now();
 renderer.setAnimationLoop(() => {
-    materialStep1.uniforms.uTimeMs.value = Date.now() - baseTime;
+    const timeDateCurrent = Date.now();
+    const timeDelta = params.animRun ? timeDateCurrent - timeDatePrev : 0.0;
+    timeAcc += timeDelta;
+    timeDatePrev = timeDateCurrent
+    materialStep1.uniforms.uTimeAccMs.value = timeAcc;
+    materialStep1.uniforms.uTimeDeltaMs.value = timeDelta;
+    materialStep1.uniforms.uPlay.value = params.animRun;
     renderer.setRenderTarget(outputRenderTarget);
     renderer.render(sceneStep1, camera);
     renderer.setRenderTarget(null);
-    renderer.render(sceneFinal, camera);
+    renderer.render(sceneFinal, camera)
     switchStep1RenderTargets();
     if (params.fpsDisplay) {
         stats.update();
@@ -118,30 +154,64 @@ const pane = new Pane({
     title: "Parameters",
     expanded: true,
 });
+pane.on("change", (_ev) => {
+    applyDisplayParams();
+});
 
 const displayFolder = pane.addFolder({
     title: "Display",
     expanded: true,
 });
+displayFolder.addBinding(params, "canvasResolution", {
+    label: "Resolution",
+    step: 1,
+    min: 1,
+    max: 100,
+    format: (v) => v + " %",
+});
+displayFolder.addBinding(params, "canvasScale", { label: "Full screen" });
+displayFolder.addBinding(params, "canvasSmooth", { label: "Canvas Smooth" });
+displayFolder.addBinding(params, "fpsDisplay", { label: "Display FPS" });
 
-displayFolder
-    .addBinding(params, "canvasResolution", {
-        label: "Resolution",
-        step: 1,
-        min: 1,
-        max: 100,
-        format: (v) => v + " %",
-    })
-    .on("change", (_ev) => {
-        applyDisplayParams();
-    });
-displayFolder
-    .addBinding(params, "canvasScale", { label: "Full screen" })
-    .on("change", (_ev) => {
-        applyDisplayParams();
-    });
-displayFolder
-    .addBinding(params, "fpsDisplay", { label: "Display FPS" })
-    .on("change", (_ev) => {
-        applyDisplayParams();
-    });
+const colorPaletteFolder = pane.addFolder({
+    title: "Color Palette",
+    expanded: true,
+})
+colorPaletteFolder.addBinding(params, "paletteLuminosity", {
+    label: "Luminosity",
+    x: { min: 0, max: 1 },
+    y: { min: 0, max: 1 },
+    z: { min: 0, max: 1 },
+});
+colorPaletteFolder.addBinding(params, "paletteContrast", {
+    label: "Contrast",
+    x: { min: 0, max: 1 },
+    y: { min: 0, max: 1 },
+    z: { min: 0, max: 1 },
+});
+colorPaletteFolder.addBinding(params, "paletteFreq", {
+    label: "Frequence",
+    x: { min: 0, max: 100 },
+    y: { min: 0, max: 100 },
+    z: { min: 0, max: 100 },
+});
+colorPaletteFolder.addBinding(params, "palettePhase", {
+    label: "Phase",
+    x: { min: 0, max: 1 },
+    y: { min: 0, max: 1 },
+    z: { min: 0, max: 1 },
+});
+
+const fireFolder = pane.addFolder({
+    title: "Fire Params",
+    expanded: true,
+});
+fireFolder.addBinding(params, "diffusionCoeff", { label: "Diffusion Coeff", min: 0.0 });
+fireFolder.addBinding(params, "verticalForce", { label: "Vertical Force", min: 1.0 });
+fireFolder.addBinding(params, "dissipationMinimum", { label: "Dissipation Minimum", min: 0.0, max: 0.1, step: 0.001 });
+
+const animationFolder = pane.addFolder({
+    title: "Animation",
+    expanded: true,
+});
+animationFolder.addBinding(params, "animRun", { label: "Run Animation" });
